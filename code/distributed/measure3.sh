a9f2b456103f5b88732fb438ba82dad21e7521b1
FrancescoMagalini1 <francesco.magalini@unitn.it>
2026-02-15 10:47:23 +0100

    other

---
+++ code/distributed/measure3.sh
@@ -0,0 +1,3 @@
+qsub -o ~/HPC/code/distributed/logs/third -e ~/HPC/code/distributed/logs/third -v ARG1=10000,ARG2=4,ARG3=3 execute2.sh
+qsub -o ~/HPC/code/distributed/logs/third -e ~/HPC/code/distributed/logs/third -v ARG1=10000,ARG2=4,ARG3=3 execute2.sh
+qsub -o ~/HPC/code/distributed/logs/third -e ~/HPC/code/distributed/logs/third -v ARG1=10000,ARG2=4,ARG3=3 execute2.sh
