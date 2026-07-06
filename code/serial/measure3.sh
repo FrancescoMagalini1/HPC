8382a17557d739533b7ebc0d64022cec580712f9
Francesco <>
2026-07-06 17:45:53 +0200

    new measures

---
+++ code/serial/measure3.sh
@@ -0,0 +1,6 @@
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
