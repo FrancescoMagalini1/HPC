20a7a7ed8b0daf5f123c8ce2676a818e7fe5a46a
Francesco <>
2026-07-06 20:47:01 +0200

    new measures

--- code/parallel/execute.sh
+++ code/parallel/execute.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # change the number of cpus as needed, more threads should require more cpus
-#PBS -l select=1:ncpus=8:mem=4gb
+#PBS -l select=1:ncpus=8:mem=20gb
 # set max execution time
-#PBS -l walltime=00:03:00
+#PBS -l walltime=01:30:00
 # imposta la coda di esecuzione
 #PBS -q shortCPUQ
 ./HPC/code/parallel/executables/linux/parallel "$ARG1" "$ARG2" "$ARG3"
--- code/serial/measure3.sh
+++ code/serial/measure3.sh
@@ -1,6 +1,9 @@
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
-qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=1200 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+#qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=2400 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=3600 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=3600 execute.sh
+qsub -o ~/HPC/code/serial/logs -e ~/HPC/code/serial/logs -v ARG1=3600 execute.sh
