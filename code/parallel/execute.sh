b69f2877ce5e488af64cba29edbfdceec77fa887
Francesco <>
2026-07-07 09:38:20 +0200

    new measures

--- code/parallel/execute.sh
+++ code/parallel/execute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # change the number of cpus as needed, more threads should require more cpus
-#PBS -l select=1:ncpus=8:mem=20gb
+#PBS -l select=1:ncpus=12:mem=20gb
 # set max execution time
 #PBS -l walltime=01:30:00
 # imposta la coda di esecuzione
