7dd5b73e50cafde187bc2469c078a6f64c1edebb
FrancescoMagalini1 <francesco.magalini@unitn.it>
2026-07-09 19:44:54 +0200

    fixes

--- code/parallel/execute.sh
+++ code/parallel/execute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # change the number of cpus as needed, more threads should require more cpus
-#PBS -l select=1:ncpus=64:mem=24gb
+#PBS -l select=1:ncpus=32:mem=24gb
 # set max execution time
 #PBS -l walltime=03:00:00
 # imposta la coda di esecuzione
