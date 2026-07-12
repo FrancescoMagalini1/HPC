d0a2cb6e1ae947e219c9ad9d5f9c5f04a7064d88
FrancescoMagalini1 <francesco.magalini@unitn.it>
2026-07-12 10:22:39 +0200

    new measures

--- code/parallel/execute.sh
+++ code/parallel/execute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # change the number of cpus as needed, more threads should require more cpus
-#PBS -l select=1:ncpus=32:mem=80gb
+#PBS -l select=1:ncpus=32:mem=240gb
 # set max execution time
 #PBS -l walltime=03:00:00
 # imposta la coda di esecuzione
--- code/parallel/measure_fast2.sh
+++ code/parallel/measure_fast2.sh
@@ -7,6 +7,3 @@ qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v 
 qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=65536,ARG2=2,ARG3=0 execute.sh
 qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=65536,ARG2=2,ARG3=0 execute.sh
 qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=65536,ARG2=2,ARG3=0 execute.sh
-qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=131072,ARG2=2,ARG3=0 execute.sh
-qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=131072,ARG2=2,ARG3=0 execute.sh
-qsub -o ~/HPC/code/parallel/logs/fast/n2 -e ~/HPC/code/parallel/logs/fast/n2 -v ARG1=131072,ARG2=2,ARG3=0 execute.sh
